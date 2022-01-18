f3d8b5e4be1ea4228031f36f32121655ccbbd1a3
Najim Abdelmoula <najim.abdelmoula@gmail.com>
2022-01-18 22:52:23 +0100

    Improve git_pull.sh bash script...

--- src/bash/git_pull.sh
+++ src/bash/git_pull.sh
@@ -1,29 +1,55 @@
 #!/bin/bash
 
+# ANSI color code variables
+red="\x1b[31m"
+blue="\x1b[34m"
+green="\x1b[32m"
+yellow="\x1b[33m"
+reset="\x1b[0m"
+
 x=$1
 
+displayIssue() {
+  case "$issue" in
+    "1")
+      echo -e "  ${red}One parameter must be specified with this script and must be an existing path${reset}"
+      echo -e "  ${red}^^^${reset}"
+      ;;
+    "2")
+      echo -e "  ${red}The specified directory does not exist${reset}"
+      ;;
+    "3")
+      echo -e "The parameter ${red}must be a path${reset} and has to be specified as follow:"
+      echo -e "              ${red}^^^^^^^^^^^^^^${reset}"
+      echo
+      echo -e "  ${green}[/]${reset}     allowed format: ${yellow}/dir1/dir2${reset}"
+      echo -e "  ${red}[X]${reset} Not allowed format: ${yellow}/dir1/dir2${reset}${red}/${reset}"
+      ;;
+  esac
+}
+
 echo
-if [ "${x:${#x}-1}" == "/"  ]; then
-  echo "This format is not allowed..."
-  echo
-  echo "  [/]     allowed format: /dir1/dir2"
-  echo "  [X] Not allowed format: /dir1/dir2/"
+if [ -z $1 ] || ! [ -z $2 ]; then
+  issue=1
+  displayIssue issue
   exit 2
 fi
 
 if ! [ -a $1 ]; then
-  echo "Only path is allowed"
+  issue=2
+  displayIssue issue
   exit 2
 fi
 
-if ! [ -z $2 ]; then
-  echo "Only one parameter is allowed"
+if [ "${x:${#x}-1}" == "/" ]; then
+  issue=3
+  displayIssue issue
   exit 2
 fi
 
+i=1
 for val in $*; do
-  i=1
-  echo "PARAM($i): $val"
+  echo -e "PARAM(${blue}$i${reset}): ${blue}$val${reset}"
   ((i++))
 done
 
@@ -31,11 +57,14 @@ echo
 for file in `ls $1`; do
   REPO="$1/$file"
   if [ -d $REPO ]; then
-    echo "[/] -> $REPO"
-    git -C $REPO checkout master
-    git -C $REPO branch
-    git -C $REPO pull
+    if [ -a $REPO"/.git" ]; then
+      echo -e "${green}[/]${reset} -> $REPO"
+      git -C $REPO checkout master
+      git -C $REPO pull
+    else
+      echo -e "${yellow}[X] $REPO isn't a Git repository...${reset}"
+    fi
   else
-    echo "[X] $REPO isn't a Git repository..."
+    echo -e "${yellow}[X] $REPO isn't a directory...${reset}"
   fi
 done
